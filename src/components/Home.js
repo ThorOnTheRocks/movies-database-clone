c2f8ea5f11ae8d3be95b1b387470eaaf70adf5f6
gianluca galota <gianluca.galota@gmail.com>
2021-08-06 18:28:29 +0100

    created Grid component and styled

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,7 @@ import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
 
 // Components
 import HeroImage from './HeroImage';
+import Grid from './Grid';
 
 // Hooks
 import { useHomeFetch } from '../hooks/useHomeFetch';
@@ -24,8 +25,12 @@ const Home = () => {
           title={state.results[0].original_title}
           text={state.results[0].overview}
         />
-        : null
-      }
+        : null}
+      <Grid header='Popular Movies'>
+        {state.results.map(movie => (
+          <div key={movie.id}>{movie.title}</div>
+        ))}
+      </Grid>
     </>
   )
 };
